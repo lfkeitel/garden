1025133e01abf294cd94cf5310ef7cad2ca1f01c
Lee Keitel <lee@keitel.xyz>
2024-03-25 15:49:18 -0500

    Don't show planting col when the table is for a specific planting

--- templates/partials/log-table.php
+++ templates/partials/log-table.php
@@ -17,7 +17,7 @@
             <th scope="col"></th>
             <th scope="col">Date</th>
             <th scope="col">Weather</th>
-            <th scope="col">Planting</th>
+            <?php if (!isset($planting_id)): ?><th scope="col">Planting</th><?php endif ?>
             <th scope="col">Time of Day</th>
             <th scope="col">Notes</th>
         </tr>
@@ -28,23 +28,25 @@
             <?php if ($this->is_logged_in() && $edit_btns): ?>
             <td class="control-cell">
                 <form method="get" action="/logs/edit/<?= $this->e($log->get_id()) ?>">
-                    <button type="submit" class="btn btn-small">Edit</button>
+                    <button type="submit" class="btn btn-small">Edit Log</button>
                 </form>
 
                 <form method="post" onsubmit="return form_confirm(this);">
                     <input type="hidden" value="delete_log" name="action">
                     <input type="hidden" value="<?= $log->get_id() ?>" name="log_id">
-                    <button type="submit" class="btn btn-small">Delete</button>
+                    <button type="submit" class="btn btn-small">Delete Log</button>
                 </form>
             </td>
             <?php endif ?>
-            <td><a href="<?= $basepath ?>/logs/<?= $log->get_id() ?>">View</a></td>
+            <td><a href="<?= $basepath ?>/logs/<?= $log->get_id() ?>">View Log</a></td>
             <td><?= $log->date->format('Y-m-d') ?></td>
             <td><?= $log->weather->temp_high ?>/<?= $log->weather->temp_low ?>&deg;C</td>
-            <?php if (!is_null($log->planting)): ?>
-            <td><a href="<?= $basepath ?>/plantings/<?= $log->planting->get_id() ?>"><?= $log->display_string() ?></a></td>
-            <?php else: ?>
-            <td><?= $log->display_string() ?></td>
+            <?php if (!isset($planting_id)): ?>
+                <?php if (!is_null($log->planting)): ?>
+                <td><a href="<?= $basepath ?>/plantings/<?= $log->planting->get_id() ?>"><?= $log->display_string() ?></a></td>
+                <?php else: ?>
+                <td><?= $log->display_string() ?></td>
+                <?php endif ?>
             <?php endif ?>
             <td><?= $log->time_of_day ?></td>
             <td><?= count($log->image_files) > 0 ? "<a href=\"{$basepath}/logs/{$log->get_id()}#images\">📷</a> " : '' ?><?= nl2br($log->notes) ?></td>
