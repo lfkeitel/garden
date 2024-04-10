d8f28ea7e83241781809ea65236bcac02e05a565
Lee Keitel <lee@keitel.xyz>
2024-04-10 07:53:40 -0500

    Make the login form a bit prettier

--- templates/login.php
+++ templates/login.php
@@ -1,14 +1,21 @@
-<?php $this->layout('main') ?>
+<?php $this->layout('main', ['styles' => ['login']]) ?>
 
-<?php if ($this->is_logged_in()): ?>
-<p>
-    You're already logged in!
-</p>
-<?php else: ?>
-<form action="<?= $basepath ?>/login" method="post">
-    Username: <input type="text" name="username" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" autofocus><br>
-    Password: <input type="password" name="password"><br><br>
+<?php if ($this->is_logged_in()) : ?>
+    <p>
+        You're already logged in!
+    </p>
+<?php else : ?>
+    <form class="login-form" action="<?= $basepath ?>/login" method="post">
+        <label>
+            <span>Username:</span>
+            <input type="text" name="username" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" autofocus>
+        </label>
 
-    <button type="submit">Login</button>
-</form>
+        <label>
+            <span>Password:</span>
+            <input type="password" name="password">
+        </label>
+
+        <button type="submit">Login</button>
+    </form>
 <?php endif ?>
